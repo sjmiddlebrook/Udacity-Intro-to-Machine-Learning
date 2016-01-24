e5bb7aa70c13c1e3acb5797a1e5507297f7b70aa
Jack Middlebrook <jack.middlebrook@gmail.com>
2016-01-24 21:12:41 +0100

    complete Lesson 2 on support vector machines

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,31 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+clf = svm.SVC(kernel='rbf', C=10000.0)
+
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time: {:.3f}s".format(time()-t0))
+
+t0 = time()
+pred = clf.predict(features_test)
+print("prediction time: {:.3f}s".format(time()-t0))
+
+count_chris = 0
+for result in pred:
+	if result == 1:
+		count_chris += 1
+
+print("prediction for Chris (1) Class: {}".format(count_chris))
+
+accuracy_score = accuracy_score(labels_test, pred)
+print(accuracy_score)
 
 #########################################################
 
